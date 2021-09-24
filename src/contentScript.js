1a883e56daaf135a10ddcb06a4e442133aa99924
RdotSilva <RyandotSilva@gmail.com>
2021-09-24 18:07:17 -0400

    Add TODO to remove logs

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,1 +1,10 @@
-console.log("contentScript.js");
+import { getTTFB, getLCP, getFID, getFCP, getCLS } from "web-vitals";
+
+// TODO: Remove these logs. For now we will log to see what type of data the API is returning
+getTTFB(console.log);
+getLCP(console.log);
+getFID(console.log);
+getFCP(console.log);
+getCLS(console.log);
+
+console.log("TEST");
