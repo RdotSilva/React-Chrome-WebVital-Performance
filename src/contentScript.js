31880df519e0c13236381d2df61c0b7f29b7fb2b
RdotSilva <RyandotSilva@gmail.com>
2021-09-25 14:53:43 -0400

    Add temporary hardcoded styles

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,7 +1,15 @@
 import { getTTFB, getLCP, getFID, getFCP, getCLS } from "web-vitals";
 
 // Info box where we will display all of the web-vitals info
-const infoDiv = document.createElement("div")
+const infoDiv = document.createElement("div");
+infoDiv.style.position = "fixed";
+infoDiv.style.left = 0;
+infoDiv.style.top = 0;
+infoDiv.style.zIndex = 0;
+infoDiv.style.backgroundColor = "black";
+infoDiv.style.color = "white";
+infoDiv.style.padding = "1rem";
+infoDiv.style.fontFamily = "Arial";
 document.body.appendChild(infoDiv);
 
 // TODO: Remove these logs. For now we will log to see what type of data the API is returning
