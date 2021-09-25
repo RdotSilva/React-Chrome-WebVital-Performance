bb80f9abb6667ad6aedc8c1146bc3b7030f2ae75
RdotSilva <RyandotSilva@gmail.com>
2021-09-25 14:53:01 -0400

    Add infoDiv to display web-vital data

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,5 +1,9 @@
 import { getTTFB, getLCP, getFID, getFCP, getCLS } from "web-vitals";
 
+// Info box where we will display all of the web-vitals info
+const infoDiv = document.createElement("div")
+document.body.appendChild(infoDiv);
+
 // TODO: Remove these logs. For now we will log to see what type of data the API is returning
 getTTFB(console.log);
 getLCP(console.log);
