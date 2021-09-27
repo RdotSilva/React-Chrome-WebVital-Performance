610687ed4a776c96de7b730828dab85e5ce4e5eb
RdotSilva <RyandotSilva@gmail.com>
2021-09-27 19:25:14 -0400

    Add listener to read data from content script

--- src/background.js
+++ src/background.js
@@ -1,1 +1,10 @@
 console.log("background.js");
+
+const data = {};
+
+// Read the message from the content script
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.type === "performance:metric") {
+    console.log(request);
+  }
+});
