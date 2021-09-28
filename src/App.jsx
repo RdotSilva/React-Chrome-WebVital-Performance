b1c921f008b2956329d60a486131097040d31a15
RdotSilva <RyandotSilva@gmail.com>
2021-09-28 15:25:59 -0400

    Add useEffect to fetch metric data

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
 
 import "./App.css";
 
 const App = () => {
+  // Get the performance metric data when loaded
+  useEffect(() => {
+    chrome.runtime.sendMessage({
+      type: "performance:metric",
+  }, (data) => {
+    console.log(data)
+  }, []);
+
   return (
     <div>
       <h1>Hello</h1>
