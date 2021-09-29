9758807f732ad25fcd883ec7034940ea53633ad0
RdotSilva <RyandotSilva@gmail.com>
2021-09-29 17:41:58 -0400

    Add useEffect to accept data

--- src/components/MetricData.jsx
+++ src/components/MetricData.jsx
@@ -1,6 +1,20 @@
+import React, { useEffect } from "react";
+
 /**
  * Component used to render MetricData taken in from Google Chrome runtime
  */
 export default function MetricData() {
+  // Get the performance metric data when loaded
+  useEffect(() => {
+    chrome.runtime.sendMessage(
+      {
+        type: "performance:metric",
+      },
+      (d) => {
+        console.log(d);
+      }
+    );
+  }, []);
+
   return <div></div>;
 }
