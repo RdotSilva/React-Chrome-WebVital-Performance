b68f2ad2dc917f5c86da101e4b17f1f22a9efc52
RdotSilva <RyandotSilva@gmail.com>
2021-09-30 16:57:16 -0400

    Add TODO for instructions to user

---
+++ src/components/About.jsx
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <div>
+      <h1>How to use: </h1>
+      {/* TODO: Add instructions for user to explain how these metrics work */}
+    </div>
+  );
+}
