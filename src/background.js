ed1a70b288de5e59266be3afc652f60418e9d289
RdotSilva <RyandotSilva@gmail.com>
2021-10-01 20:47:26 -0400

    Fix TODO to remove log

--- src/background.js
+++ src/background.js
@@ -5,8 +5,6 @@ const data = {};
 // Read the message from the content script
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.type === "performance:metric") {
-    console.log(request);
-    // TODO: Remove log and render data to React
     // Get the URL of the tab
     const tab = sender.tab.url.toString();
     data[tab] = data[tab] || {};
