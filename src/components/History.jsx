c1e5ad771ba85283135d1a57125b93856fdf7c86
RdotSilva <RyandotSilva@gmail.com>
2021-10-02 17:11:20 -0400

    Add TODO to cache historical data

---
+++ src/components/History.jsx
@@ -0,0 +1,8 @@
+export default function History() {
+  return (
+    <div>
+      <h1>Historic Data</h1>
+    </div>
+    //   TODO: Add historical data for pages. Going to need to cache this data
+  );
+}
