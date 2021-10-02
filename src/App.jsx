fb4b6ad4c87f871dde6e3f98452da89a08e5ff09
RdotSilva <RyandotSilva@gmail.com>
2021-10-01 20:50:06 -0400

    Add link for About page

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./App.css";
 import MetricData from "./components/MetricData";
+import { Link } from "react-router-dom";
 
 const App = () => {
-  return <MetricData />;
+  return (
+    <>
+      <Link to="/about" />
+      <MetricData />;
+    </>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById("app"));
