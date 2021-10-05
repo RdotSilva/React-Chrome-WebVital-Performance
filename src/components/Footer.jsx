05dbe27e85ca6a55f77a730737fc3f9179c0987e
RdotSilva <RyandotSilva@gmail.com>
2021-10-05 15:44:59 -0400

    Add Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div>
+      <footer>
+        Copyright &copy; {new Date().getFullYear()} Web Vital Performance
+        Extension
+      </footer>
+    </div>
+  );
+}
